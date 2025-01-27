35b5a7a1e4122ea64f817cbc764613523ee5620a
Elvira Sumarokava <40741867+eltimka@users.noreply.github.com>
2025-01-27 12:34:46 -0500

    Creating DTO objects

---
+++ src/main/java/com/eltimka/awssqsdemo/dto/NotificationDto.java
@@ -0,0 +1,11 @@
+package com.eltimka.awssqsdemo.dto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming( PropertyNamingStrategy.SnakeCaseStrategy.class)
+public record NotificationDto(String id,
+                              String subject,
+                              String text,
+                              String recipientUid) {
+}
