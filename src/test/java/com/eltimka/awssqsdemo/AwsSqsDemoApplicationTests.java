0c47579fbcc2a465ed5cf2b11000609329c1255c
Elvira Sumarokava <40741867+eltimka@users.noreply.github.com>
2025-01-27 12:08:57 -0500

    Creating a project structure

---
+++ src/test/java/com/eltimka/awssqsdemo/AwsSqsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eltimka.awssqsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
